a2341aecb16ed086fc1728e9490ae4f4765ffedd
Pete Lawrence <petejlawrence@icloud.com>
2020-06-19 20:21:25 +0100

    Updates

---
+++ precache-manifest.a7a60e3e3b14cc4c2bca87c7fc06f672.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "34b8d1226ba9c62c365fc73b2938b65f",
+    "url": "/index.html"
+  },
+  {
+    "revision": "1989ac7e64ab9fa95ffb",
+    "url": "/static/css/2.de424728.chunk.css"
+  },
+  {
+    "revision": "4d1f46ee69c44fc85a72",
+    "url": "/static/css/main.d90ea5e8.chunk.css"
+  },
+  {
+    "revision": "1989ac7e64ab9fa95ffb",
+    "url": "/static/js/2.df7d2e19.chunk.js"
+  },
+  {
+    "revision": "5ac48c47bb3912b14c2d8de4f56d5ae8",
+    "url": "/static/js/2.df7d2e19.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "4d1f46ee69c44fc85a72",
+    "url": "/static/js/main.b8e803bb.chunk.js"
+  },
+  {
+    "revision": "524e8255ad3f6fad7e54",
+    "url": "/static/js/runtime-main.5f172887.js"
+  }
+]);
